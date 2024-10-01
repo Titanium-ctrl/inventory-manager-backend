bc2c3d87edd454a4ab3f4af96d06c0c4293a2b9b
Titanium-ctrl <whwebworks@gmail.com>
2024-10-01 22:03:58 +0100

    implement loading from .env file and auth middleware

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"ucrs.com/inventory-manager/backend/internal/database"
+	"github.com/joho/godotenv"
 	"ucrs.com/inventory-manager/backend/internal/routes"
+	"ucrs.com/inventory-manager/backend/middleware"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
 	app := fiber.New()
 
-	database.Connect()
+	app.Use(middleware.DBClientMiddleware)
+
 	routes.SetupRoutes(app)
 
 	app.Listen(":3000")

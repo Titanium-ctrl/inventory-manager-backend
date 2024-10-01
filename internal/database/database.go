5ea1a7983361a112a07dea1ea8769cbcad970a21
Titanium-ctrl <whwebworks@gmail.com>
2024-10-01 22:04:20 +0100

    create functions for creating supabase client and fetching user id

---
+++ internal/database/database.go
@@ -0,0 +1,41 @@
+package database
+
+import (
+	"os"
+	"strings"
+
+	"github.com/google/uuid"
+	"github.com/supabase-community/gotrue-go/types"
+	"github.com/supabase-community/supabase-go"
+)
+
+func CreateClient(jwt string) *supabase.Client {
+	// TODO: Create database client
+
+	//My API_URL and API_KEY variables need to go into a .env file. How do i fetch them?
+
+	API_URL := os.Getenv("API_URL")
+	API_KEY := os.Getenv("API_KEY")
+	client, err := supabase.NewClient(API_URL, API_KEY, &supabase.ClientOptions{})
+	if err != nil {
+		panic(err)
+	}
+	//client.Functions.TokenAuth(jwt)
+	jwt = strings.Replace(jwt, "Bearer ", "", 1)
+
+	session := types.Session{
+		AccessToken: jwt,
+	}
+	client.UpdateAuthSession(session)
+	// client.Auth.WithToken(jwt)
+
+	return client
+}
+
+func FetchUserID(client *supabase.Client) uuid.UUID {
+	clientdetails, err := client.Auth.GetUser()
+	if err != nil {
+		panic(err)
+	}
+	return clientdetails.ID
+}

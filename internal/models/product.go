d16bb70e4491e0a29c5cea4bde0d60cc49113bdb
Titanium-ctrl <whwebworks@gmail.com>
2024-10-01 22:05:25 +0100

    refined struct for products - allowing some values to be empty and removed in json

--- internal/models/product.go
+++ internal/models/product.go
@@ -1,12 +1,17 @@
-package models  
-  
-import "time"  
-  
-type Product struct {  
-    ID          uint      `json:"id"`  
-    Name        string    `json:"name"`  
-    Description string    `json:"description"`  
-    Price       float64   `json:"price"`  
-    CreatedAt   time.Time `json:"created_at"`  
-    UpdatedAt   time.Time `json:"updated_at"`  
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Product struct {
+	ID          uuid.UUID `json:"id,omitempty"`
+	UserID      uuid.UUID `json:"user_id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description,omitempty"`
+	Price       float64   `json:"price"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
